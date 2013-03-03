36efe62872c0b64f4435e8643d8a8d0d48b9d9ef
Goswin von Brederlow <goswin-v-b@web.de>
2013-03-03 10:53:12 +0100

    Import primitive memcpy().

---
+++ include/memcpy.h
@@ -0,0 +1,28 @@
+/* memcpy.h - copy memory */
+/* Copyright (C) 2013 Goswin von Brederlow <goswin-v-b@web.de>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef MOOSE_MEMCPY_H
+#define MOOSE_MEMCPY_H
+
+#include <stddef.h>
+
+extern "C" {
+    void * memcpy(void *dest, const void *src, size_t n);
+}
+
+#endif // #ifndef MOOSE_MEMCPY_H
